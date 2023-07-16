e845e9754f9eca1bbd61eb4d363714ac2117ff46
AmaanAlii <amaans.officialmail@gmail.com>
2023-07-16 15:33:18 +0530

    Added most all the styling and handled most of the errors

--- src/components/WeatherApp/WeatherApp.js
+++ src/components/WeatherApp/WeatherApp.js
@@ -10,12 +10,16 @@ import {
   WiThunderstorm,
   WiFog,
   WiNightClear,
+  WiThermometer,
+  WiRaindrop,
 } from "react-icons/wi";
+import { FaMapMarkerAlt } from "react-icons/fa";
 
 function WeatherApp() {
   const API_KEY = "2859cec3cc613ce0dd1615485d75864a";
 
   const fetchCoordinates = async (cityName) => {
+    // this function fetches coordinates based on the city name
     try {
       const url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=100&appid=${API_KEY}`;
       const response = await axios.get(url);
@@ -27,6 +31,7 @@ function WeatherApp() {
   };
 
   const fetchWeatherData = async (lat, lon) => {
+    // This function uses latitude and longitude to get better results
     try {
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
       const response = await axios.get(url);
@@ -39,6 +44,7 @@ function WeatherApp() {
 
   const [cityName, setCityName] = useState("");
   const [weatherData, setWeatherData] = useState(null);
+  const [isWeatherDataVisible, setWeatherDataVisible] = useState(false);
 
   const handleLocationChange = (event) => {
     setCityName(event.target.value);
@@ -51,14 +57,14 @@ function WeatherApp() {
         setWeatherData("empty"); // Set the error message for empty city name
         return; // Exit the function to prevent further API calls
       }
-      const cityData = await fetchCoordinates(cityName);
+      const cityData = await fetchCoordinates(cityName); // To fetch coordinates based on the city name
       if (cityData && cityData.length > 0) {
         const cityWeather = await fetchWeatherData(
           cityData[0].lat,
           cityData[0].lon
         );
-        console.log(cityWeather);
         setWeatherData(cityWeather);
+        setWeatherDataVisible(true);
       } else {
         console.error(`Cannot find a city named: ${cityName}`);
         setWeatherData("error"); // Set the error message in the state
@@ -129,21 +135,55 @@ function WeatherApp() {
         </div>
         <div className="button">
           <button onClick={handleFetchWeather}>Check Weather</button>
-
-          {weatherData && typeof weatherData !== "string" && (
-            <div>
-              <h2>{weatherData.name}</h2>
-              <p>{weatherData.weather[0].description}</p>
-              <p>Temperature: {weatherData.main.temp}</p>
-              <p>Feels like: {weatherData.main.feels_like}</p>
-              <p>Humidity: {weatherData.main.humidity}</p>
-              <div className="weather-icon-container">
-                {getWeatherIcon(weatherData.weather[0].icon)}
-              </div>
-            </div>
-          )}
         </div>
       </div>
+      {weatherData && typeof weatherData !== "string" && (
+        <div className="data-container">
+          <div className="weather-icon-container">
+            {getWeatherIcon(weatherData.weather[0].icon)}
+          </div>
+          <h1>{Math.round(weatherData.main.temp - 273.15)}&deg;C</h1>
+          <h3>{weatherData.weather[0].description.toUpperCase()}</h3>
+          <div className="heading">
+            <h2>
+              <FaMapMarkerAlt />
+              {weatherData.name}, {weatherData.sys.country}
+            </h2>
+          </div>
+
+          <div className="lower-container">
+            <div className="horizontal-line"></div>
+            <div className="info-container">
+              <div className="feels-container">
+                <WiThermometer size="60" color="blue" />
+                <div className="additional-text">
+                  <span>
+                    <strong className="main-text">
+                      {Math.round(weatherData.main.feels_like - 273.15)}
+                      &deg;C
+                    </strong>
+                    <br />
+                    <span className="small-text">Feels like</span>
+                  </span>
+                </div>
+              </div>
+              <div className="vertical-line"></div>
+              <div className="humidity-container">
+                <WiRaindrop size="60" color="blue" />
+                <div className="additional-text">
+                  <span>
+                    <strong className="main-text">
+                      {weatherData.main.humidity}%
+                    </strong>
+                    <br />
+                    <span className="small-text">Humidity</span>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
